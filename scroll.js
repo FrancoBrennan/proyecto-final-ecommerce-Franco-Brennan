d7310e4da7c5ef172c1e5bf19ef1e8bb54c148a3
Franco Brennan <franco.brennan1@gmail.com>
2024-12-14 19:24:06 -0300

    Feat: Contact page redesign

--- scroll.js
+++ scroll.js
@@ -1,13 +1,14 @@
-var header = document.getElementById('Header')
+var header = document.getElementById('Header');
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function() {
     var scrollY = window.scrollY;
-    if(scrollY>0){
-        header.style.backgroundColor = '#201f1f';
+    var screenWidth = window.innerWidth;
 
-    }
-    else{
-        header.style.backgroundColor = 'transparent';
-    }
-
-})
+    if (screenWidth >= 992) { // Verifica el ancho de la pantalla
+        if (scrollY > 0) {
+            header.style.backgroundColor = '#201f1f';
+        } else {
+            header.style.backgroundColor = 'transparent';
+        }
+    } 
+});
